4f05c5b025044fd451b37fc5d434c6c03f72a444
Davide <davidemacchi97@gmail.com>
2018-09-30 18:25:07 +0200

    base

--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
-function preload(){
-  // put preload code here
-  fill(155,155,155);
-  background(200,200);
-}
+var diametro = 420;
+var inc = 1;
+
 
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+    strokeWeight(2);
+
 }
 
 function draw() {
-  // put drawing code here
+    background(0, 0, 0);
+    noFill();
+    noStroke();
+
+
+    for (var i = 0; i < 180; i = i + 1.5) {
+        stroke(175,60,60);
+        line(width/2+diametro/2, height/2,
+            width/2+diametro/2-(diametro*cos(i)*cos(i)),
+
+            height/2-(diametro*cos(i)*sin(i)));
+
+    }
 }
